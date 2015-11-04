695b59793fc96a32402f57bcedb305307ba89757
rsanchez <r.sanchezavalos@gmail.com>
2015-11-03 20:37:49 -0600

    django app

---
+++ venve/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rsanchez/workspace/ObservatoriodeVivienda/web-obs/venve/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ webapp/urls.py
@@ -0,0 +1,65 @@
+"""webapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from zinnia.sitemaps import TagSitemap
+from zinnia.sitemaps import EntrySitemap
+from zinnia.sitemaps import CategorySitemap
+from zinnia.sitemaps import AuthorSitemap
+from django.conf.urls import patterns, include, url, static
+from django.conf import settings
+from django.conf.urls.static import static
+admin.autodiscover()
+
+
+sitemaps = {'tags': TagSitemap,
+            'blog': EntrySitemap,
+            'authors': AuthorSitemap,
+            'categories': CategorySitemap,}
+
+blog_urls = [
+    url(r'^', include('zinnia.urls.capabilities')),
+    url(r'^search/', include('zinnia.urls.search')),
+    url(r'^sitemap/', include('zinnia.urls.sitemap')),
+    url(r'^trackback/', include('zinnia.urls.trackback')),
+    url(r'^blog/tags/', include('zinnia.urls.tags')),
+    url(r'^blog/feeds/', include('zinnia.urls.feeds')),
+    url(r'^blog/random/', include('zinnia.urls.random')),
+    url(r'^blog/authors/', include('zinnia.urls.authors')),
+    url(r'^blog/categories/', include('zinnia.urls.categories')),
+    url(r'^blog/comments/', include('zinnia.urls.comments')),
+    url(r'^blog/', include('zinnia.urls.entries')),
+    url(r'^blog/', include('zinnia.urls.archives')),
+    url(r'^blog/', include('zinnia.urls.shortlink')),
+    url(r'^blog/', include('zinnia.urls.quick_entry'))
+]
+
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^weblog/', include('zinnia.urls', namespace='zinnia')),
+    url(r'^comments/', include('django_comments.urls')),
+    #url(r'^', include(blog_urls, namespace='zinnia')),
+    ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+"""urlpatterns += patterns(
+    'django.contrib.sitemaps.views',
+    url(r'^sitemap.xml$', 'index',
+        {'sitemaps': sitemaps}),
+    url(r'^sitemap-(?P<section>.+)\.xml$', 'sitemap',
+        {'sitemaps': sitemaps}),)
+"""
